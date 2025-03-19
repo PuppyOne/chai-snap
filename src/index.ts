3da3bf37d5375ea26bfa5def447267ddaf185b39
PuppyOne <zheng_xz@qq.com>
2025-03-19 22:36:24 +0800

    feat: add `snapshot` method for chai (#3)
    - Add new method 'snapshot' to the Chai Assertion
    - Integrate snap-shot-it library for snapshot management
    
    - Add type definitions for Chai.ChaiStatic and Chai.ChaiUtils
    - Improve code structure by renaming the main function to chaiSnap

---
+++ src/index.ts
@@ -0,0 +1,8 @@
+import snapshot from 'snap-shot-it';
+
+export default function chaiSnap(_chai:Chai.ChaiStatic, utils:Chai.ChaiUtils) {
+  utils.addMethod(_chai.Assertion.prototype, 'snapshot', function (this:Chai.Assertion) {
+    const obj = utils.flag(this, 'object');
+    snapshot(obj);
+  }); 
+}
