38eafcb3b0b1ea757bf98d59639c65bab1ba84e7
PuppyOne <zheng_xz@qq.com>
2025-03-20 18:21:47 +0800

    test(snapshots): enhance snapshot testing for different data types (#17)
    - Add snapshot tests for string, number, boolean, and object types
    - Skip null and undefined tests due to upstream unsupporting

--- test/index.test.ts
+++ test/index.test.ts
@@ -1,10 +1,42 @@
 import { expect, use } from 'chai';
 import chaiSnapshot from '../src/index.js';
+import chaiAsPromised from 'chai-as-promised';
 
 use(chaiSnapshot);
 
+use(chaiAsPromised);
+
 describe('chai-snap', () => {
-  it('should pass snapshot', () => {
-    expect('test').to.snapshot();
+  context('matching different types', () => {
+    it('should match string snapshot', () => {
+      expect('test').to.snapshot();
+    });
+
+    it('should match number snapshot', () => {
+      expect(1).to.snapshot();
+    });
+
+    it('should match boolean snapshot', () => {
+      expect(true).to.snapshot();
+    });
+
+    it.skip('should match null snapshot', () => {
+      expect(null).to.snapshot();
+    });
+
+    it.skip('should match undefined snapshot', () => {
+      expect(undefined).to.snapshot();
+    });
+
+    it('should match object snapshot', () => {
+      expect({ id: 1, I: 'am', a: Object }).to.snapshot();
+    });
+  });
+
+  context('cooperating with other plugins', () => {
+    it('should cooperate with chai-as-promised', () => {
+      expect(Promise.resolve('test')).to.eventually.fulfilled;
+      expect('test').to.snapshot();
+    });
   });
 });
