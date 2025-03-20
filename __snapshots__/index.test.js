5f2c15dc9e9edfad482c4a96d5234e9f3cc18517
PuppyOne <zheng_xz@qq.com>
2025-03-20 18:20:19 +0800

    test(snapshots): enhance snapshot testing for different data types
    - Add snapshot tests for string, number, boolean, and object types
    - Skip null and undefined tests due to upstream unsupporting

--- __snapshots__/index.test.js
+++ __snapshots__/index.test.js
@@ -1,3 +1,16 @@
-exports['chai-snap should pass snapshot 1'] = `
+exports['chai-snap matching different types should match string snapshot 1'] = `
+test
+`
+
+exports['chai-snap matching different types should match number snapshot 1'] = 1
+
+exports['chai-snap matching different types should match boolean snapshot 1'] = true
+
+exports['chai-snap matching different types should match object snapshot 1'] = {
+  "id": 1,
+  "I": "am"
+}
+
+exports['chai-snap cooperating with other plugins should cooperate with chai-as-promised 1'] = `
 test
 `
